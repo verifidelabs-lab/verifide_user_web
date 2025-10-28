e10a227db238a0c4e0e85b06989b104d3c9b5e0c
Rahul <Rahul.verifide@gmail.com>
2025-10-29 01:04:37 +0530

    save

--- src/components/hooks/axiosProvider.js
+++ src/components/hooks/axiosProvider.js
@@ -2,11 +2,11 @@ import axios from "axios";
 import { getCookie, removeCookie } from "../utils/cookieHandler";
 import { CiNoWaitingSign } from "react-icons/ci";
 
-const isLive = false;
-// const isLive = true;
+// const isLive = false;
+const isLive = true;
 // export const BaseUrl = "https://verifide.xyz/"
-// export const BaseUrl = "https://dev-verifide.verifide.xyz/";
-export const BaseUrl = "http://localhost:3000/";
+export const BaseUrl = "https://dev-verifide.verifide.xyz/";
+// export const BaseUrl = "http://localhost:3000/";
 export const apiUrl = isLive
   ? `${BaseUrl}api/v1/`
   : "http://192.168.1.9:5004/api/v1/";
