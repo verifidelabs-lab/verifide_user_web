9f68e4967e65e7af82fc22559049413afab0ed6b
Rahul <Rahul.verifide@gmail.com>
2025-11-17 11:21:38 +0530

    sve

--- src/components/hooks/axiosProvider.js
+++ src/components/hooks/axiosProvider.js
@@ -5,8 +5,8 @@ import { toast } from "sonner";
 
 const isLive = false;
 // const isLive = true;
-// export const BaseUrl = "https://verifide.xyz/"
-export const BaseUrl = "https://dev-verifide.verifide.xyz/";
+export const BaseUrl = "https://verifide.xyz/"
+// export const BaseUrl = "https://dev-verifide.verifide.xyz/";
 // export const BaseUrl = "http://localhost:3000/";
 export const apiUrl = isLive
   ? `${BaseUrl}api/v1/`
