759dedc1683d0b9e1817ae3091ada711163f4e79
Rahul <Rahul.verifide@gmail.com>
2025-12-02 13:10:49 +0530

    asve

--- src/pages/Assessment/index.js
+++ src/pages/Assessment/index.js
@@ -1,26 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { getCookie } from '../../components/utils/cookieHandler';
-import RecruiterAssessment from './RecruiterAssessment';
-import StudentAssessment from './components/StudentAssessment'; 
+import React, { useState, useEffect } from "react";
+import { getCookie } from "../../components/utils/cookieHandler";
+import RecruiterAssessment from "./RecruiterAssessment";
+import StudentAssessment from "./components/StudentAssessment";
 
 const Index = () => {
-   const [accessMode, setAccessMode] = useState(null);
+  const [accessMode, setAccessMode] = useState(null);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       const mode = getCookie("ACCESS_MODE");
       setAccessMode(mode);
-     }, 300);
+    }, 300);
 
     return () => clearTimeout(timer);
-  }, []); 
+  }, []);
   return (
     <div>
-      {accessMode === '6' ? (
-        <RecruiterAssessment />
-      ) : (
-        <StudentAssessment />
-      )}
+      {accessMode === "6" ? <RecruiterAssessment /> : <StudentAssessment />}
     </div>
   );
 };
